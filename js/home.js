bec775f5f2c685bf1011039cbfb0ac2cd5ea2efb
Akhou Uribe <105692560+khouu@users.noreply.github.com>
2023-11-03 18:05:16 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,14 +1,18 @@
-const nav = document.querySelector('nav');
-const house = document.getElementById('houses');
-const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("hamburger-nav-menu");
-const readyButton = document.getElementById('ready');
+const nav           = document.querySelector('nav'),
+      house         = document.getElementById('houses'),
+      hamburger     = document.getElementById("hamburger"),
+      navMenu       = document.getElementById("hamburger-nav-menu"),
+      readyButton   = document.getElementById('ready');
+
+const scrollSpeed   = 1,
+      minScale      = 1,
+      maxScale      = 1.25;      
+
 let scrollAmount = window.innerHeight;
 
-let isScrolling = false;
-let translateY = 60;
-let width = 60;
-const scrollSpeed = 1;
+let isScrolling = false,
+    translateY  = 60,
+    width       = 60;
 
 function updateScrollAmount() {
     scrollAmount = window.innerHeight;
@@ -22,36 +26,47 @@ function scrollPageSmoothly(top) {
     });
 }
 
-//this took so much time please dont touch this
-function handleScroll() {
-    if (!isScrolling) {
-        translateY = 60 + (window.scrollY / (window.innerHeight - 100)) * 70 * scrollSpeed;
-        width = 60 + (window.scrollY / (window.innerHeight - 100)) * 10 * scrollSpeed;
+function handleNavigationOnScroll() {
+    if ((window.scrollY > window.innerHeight - 100))
+        return nav.style.top = '0'
 
-        const minScale = 1;
-        const maxScale = 1.25;
-        const scale = minScale + (maxScale - minScale) * (window.scrollY / (window.innerHeight - 100));
+    nav.style.top           = '-100%';
+    navMenu.style.left      = '-80%';
+    hamburger.classList.remove("active");
+}
 
+function handleHousesOnScroll() {
+    if (isScrolling) return isScrolling = false;
+
+    let scaleFactor = (window.scrollY / (window.innerHeight - 100)) * scrollSpeed,
+        scale       = minScale + (maxScale - minScale) * scaleFactor / scrollSpeed;
+    
+    translateY  = 60 + scaleFactor * 87;
+    width       = 60 + scaleFactor * 10;
+
+    if (window.scrollY < (window.innerHeight * 1.3 - 200)) {
         house.style.transform = `translateY(${translateY}vh) scale(${scale})`;
         house.style.width = `${width}vw`;
+        if(window.scrollY > (window.innerHeight - 100)) {
+            house.style.pointerEvents = 'auto';
+        }
+        else house.style.pointerEvents = 'none';
     }
-    nav.style.top = window.scrollY > window.innerHeight - 100 ? '0' : '-100%';
-    isScrolling = false;
 }
 
 function toggleHamburger() {
     hamburger.classList.toggle("active");
-    if (hamburger.classList.contains("active")) {
-        navMenu.style.left = '0';
-    } 
-    else {
-        navMenu.style.left = '-80%';
-    }
+    if (!(window.scrollY > window.innerHeight - 100)) 
+        return
+    if (hamburger.classList.contains("active"))
+        return navMenu.style.left = '0';
+    return navMenu.style.left = '-80%';
 }
 
 readyButton.addEventListener('click', () => {
     scrollPageSmoothly(scrollAmount);
 });
 
-window.addEventListener('scroll', handleScroll);
+window.addEventListener('scroll', handleHousesOnScroll);
+window.addEventListener('scroll', handleNavigationOnScroll);
 window.addEventListener('resize', updateScrollAmount);
